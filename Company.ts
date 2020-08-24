b94e795f447d4ca2d343ad09491a39bce52623d6
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-08-24 22:18:10 +0530

    first commit

---
+++ Company.ts
@@ -0,0 +1,29 @@
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class Company implements Mappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+    <h1><strong>Company Name:</strong> ${this.name}</h1>
+    <p><strong>CatchPhrase:</strong> ${this.catchPhrase}</p>
+    </div>
+    `;
+  }
+}
